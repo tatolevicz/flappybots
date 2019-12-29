aaee0c4f96c32a2099f919d3b9990743a7a19d22
Tato Levicz <leviczios@gmail.com>
2019-12-29 15:27:14 -0300

    best agent file + minor change splash

--- Classes/Splash.hpp
+++ Classes/Splash.hpp
@@ -8,14 +8,13 @@
 #define Splash_hpp
 
 #include "cocos2d.h"
-using namespace cocos2d;
 
 class Splash : public cocos2d::Scene
 {
 public:
     static cocos2d::Scene* createScene();
     virtual bool init();
-    void setupScreen(Vec2 origin, Size visibleSize);
+    void setupScreen(cocos2d::Vec2 origin, cocos2d::Size visibleSize);
     // implement the "static create()" method manually
     CREATE_FUNC(Splash);
 };
